8c3c759436ea082661601d8162b93fadd90f6856
Kristy Hamlin <khaml004@fiu.edu>
2023-10-08 15:15:29 -0400

    Kristy leaving off E4 for now

--- nachos-3.4/code/threads/elevator.cc
+++ nachos-3.4/code/threads/elevator.cc
@@ -1,16 +1,58 @@
 #include "copyright.h"
 #include "system.h"
 
+typedef struct Person {
+    int id;
+    int atFloor;
+    int toFloor;
+} Person;
+
+
+void ElevatorThread(int numFloors){
+    printf("Elevator function invoked! Elevator has %d floors.\n", numFloors);
+}
 
 void Elevator(int numFloors){
+    //Every time the elevator reaches a new floor, it needs to do two things.
+    //First, it needs to tell all the threads what floor it is at so all the
+    //floors can get off if they need to.
+    //Secondly, all the waiting floor can get on if they want to.
 
-printf("Elevator function invoked! Elevator has %d floors.\n", numFloors);
+    //Create Elevator Thread
+    Thread *t = new Thread("Elevator");
+    t->Fork(ElevatorThread, numFloors);
+
+
+    
 
 
 }
 
+//Fork() can only pass one integer as the second argument. 
+void PersonThread(int person){
+
+    Person *p = (Person *)person;
+
+    printf("Person %d wants to go from floor %d to %d\n", p->id, p->atFloor, p->toFloor);
+
+}
+
+int getNextPersonID(){
+    return 0;
+}
+
 void ArrivingGoingFromTo(int atFloor, int toFloor){
 
-printf("Person wants to go from floor %d to %d\n", atFloor, toFloor);
+   
+    
+    //Create Person struct:
+    Person *p = new Person;
+    p->id = getNextPersonID();
+    p->atFloor = atFloor;
+    p->toFloor = toFloor;
+
+    //Create Person thread
+    Thread *t = new Thread("Person " + p->id);
+    t->Fork(PersonThread, (int)p);
 
 }
