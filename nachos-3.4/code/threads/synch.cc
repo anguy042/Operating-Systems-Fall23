095af1e986664ac2217b6dac26eb67f592c0e5aa
Trang Tran <trantrang91@yahoo.com>
2023-10-10 19:50:18 -0500

    t

--- nachos-3.4/code/threads/synch.cc
+++ nachos-3.4/code/threads/synch.cc
@@ -199,8 +199,10 @@ void Condition::Signal(Lock* conditionLock) {
 void Condition::Broadcast(Lock* conditionLock) { 
 
     IntStatus oldLevel = interrupt->SetLevel(IntOff);	// disable interrupts
-    ASSERT(conditionLock->isHeldByCurrentThread()); 
-
+    while (!conditionLock->isHeldByCurrentThread()) { 			// lock not available
+        queue->Append((void *)currentThread);	// so go to sleep
+        currentThread->Sleep();
+    } 
     // Dequeue all threads in the queue one-by-one
     Thread *thread;
     thread = (Thread *)queue->Remove();
